7d2619a86c19b9ea58ac35d249b4bba7a3ba514f
Kyle Jones <kyle074613@gmail.com>
2020-02-27 18:52:00 -0800

    testing travis

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+var express = require("express");
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
